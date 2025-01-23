a9bd84fefc74d74815ec6e040de9dcfa850cbaa8
Gungun Dua <gungun30dua@gmail.com>
2025-01-23 23:59:41 +0530

    Add mayoor_main file

---
+++ src/component/Home/index.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import Wrapper from './style'
+import HomeList from './Homelist'
+
+const Home = ({user}) => {
+
+    const [index, setIndex] = useState(1)
+    const [tabs, setTabs] = useState([{
+        id : 1,
+        title : "Home"
+    },{
+        id : 2,
+        title : "Students"
+    },{
+        id : 3,
+        title : "RO"
+    },{
+        id : 4,
+        title : "LO"
+    },{
+        id : 5,
+        title : "AC" 
+    }])
+
+  return (
+    <Wrapper>
+        <div className="screen">
+            {
+                index === 1
+                ? <HomeList user={user} setIndex={setIndex} />
+                : (index === 2
+                    ? "Students List"
+                    : (index === 3
+                        ? "RO Lists"
+                        : (index === 4
+                            ? "LO Lists"
+                            : "AC Lists"
+                    )
+                )
+            )}
+        </div>
+        <div className="bottom">
+            {
+                tabs.map(tab => <input className={index === tab.id ? 'active' : ''} type="button" value={tab.title} onClick={e => setIndex(tab.id)} />)
+            }
+        </div>
+    </Wrapper>
+  )
+}
+
+export default Home
---
+++ src/component/RO_List/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Wrapper from './style'
+
+const ROlist = () => {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  )
+}
+
+export default ROlist
